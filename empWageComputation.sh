56b5356f7808d5865f826ca41f0fd4c3f70166f1
hhmadan <hhmadan03@gmail.com>
2022-09-21 20:30:11 +0530

    Added: UC4_SolvingusingCaseStatement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,15 +1,18 @@
-isPartTime=1
-isFullTime=2
-empRatePerHr=20
-randomCheck=$((RANDOM%3))
-if [ $isFullTime -eq $randomCheck ];
-then
-	empHrs=8;
-elif [ $isPartTime -eq $randomCheck ];
-then 
-	empHrs=4
-else
-	empHrs=0
-fi
-	salary=$(($empRatePerHr*$empHrs))
-	echo "salary=$salary"
+isPartTime=1;
+isFullTime=2;
+empCheck=$((RANDOM%3));
+empRatePerHr=20;
+
+case $empCheck in
+                $isFullTime)
+                        empHrs=8
+                        ;;
+                $isPartTime)
+                        empHrs=4
+                        ;;
+                *)
+                	empHrs=0
+                        ;;
+esac
+salary=$(($empRatePerHr*$empHrs))
+echo $salary
