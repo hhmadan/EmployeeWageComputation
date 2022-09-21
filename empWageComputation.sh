8d4b8cba778cf9f63bf049a1355da7de1225e25c
hhmadan <hhmadan03@gmail.com>
2022-09-21 23:18:32 +0530

    Added: UC7_CalculateWageUsingFunction

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,31 +1,32 @@
-#UC6_CalculatingWagesForaMonthUsingWhileLoop
+#UC7_CalculateWageUsingFunction
+
 is_PartTime=1;
 is_FullTime=2;
-MaxHrs_inMonth=100;
+MaxHrs_inMonth=10;
 empRatePerHr=20;
 numWorkingDays=20;
 totalEmpHr=0;
 totalWorkingDays=0;
 
+function getWorkingHours() {
+   case $1 in
+       $is_FullTime)
+         empHrs=8
+          ;;
+       $is_PartTime)
+         empHrs=4
+          ;;
+       *)
+         empHrs=0
+          ;;
+   esac
+ echo $empHrs;
+}
 
-while [[ $totalEmpHr -lt $MaxHrs_inMonth && $totalWorkingDays -lt $numWorkingDays ]]
+while [[ $totalempHrs -lt $MaxHrs_inMonth && $totalWorkingDays -lt $numWorkingDays ]]
 do
-	((totalWorkingDays++))
-	empCheck=$((RANDOM%3));
-	
-	case $empCheck in
-		$is_FullTime)
-			empHrs=8
-          		;;
-		$is_PartTime)
-         		empHrs=4
-          		;;
-       		*)
-         		empHrs=0
-          		;;
-   	esac
-totalEmpHr=$(($totalEmpHr + $empHrs)) 
+      ((totalWorkingDays++))
+      empHrs="$( getWorkingHours $((RANDOM%3)) )"
+      totalWorkHrs=$(($totalempHrs+$empHrs))
 done
-
-totalSalary=$(($totalEmpHr+$empRatePerHr));
-echo "Total Salary is: $totalSalary"
+echo "Total Working Hours: $totalWorkHrs"
